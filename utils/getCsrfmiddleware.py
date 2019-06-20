011732d760671606ede936d25dfa929d8b68fe0b
xiaowang <2081896995@qq.com>
2019-06-20 11:18:59 +0800

    修改自定义中间件获取csrf_token

--- utils/getCsrfmiddleware.py
+++ utils/getCsrfmiddleware.py
@@ -6,7 +6,8 @@ class GetMiddleware(MiddlewareMixin):
     """
     自定义中间件获取csrf_token
     """
-    # process_request是请求预处理发送给前端的，process_response是获取将获取的数据进行预处理，然后给后端
+    # process_request是将前端获取的csrftoken验证，process_response是将处理完成的response返回给前端
     # 需要注意 这里的逻辑有点不同
     def process_request(self, request):
-        get_token(request)
+        get_token(request) # 验证csrf_token
+
